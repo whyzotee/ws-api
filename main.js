cf950d4afc1ec3e86d77e639e94d20074d86c3b2
Z0TEExt <zenlektomyum@gmail.com>
2023-03-16 16:08:56 +0700

    feat: delete channel on empty room and send status to client

--- main.js
+++ main.js
@@ -39,8 +39,7 @@ app.get("/ws", (req, res) => {
 app.get("/msg/:id", (req, res) => {
   let room_id = req.params.id;
   if (!(room_id in tempData)) {
-    console.log(1);
-    res.status(200).send('ว่าง');
+    res.status(404).send('[ws-serverAPI]: No have Room');
   } else {
     let full_room = [];
     for (let i in all_key) {
@@ -49,12 +48,24 @@ app.get("/msg/:id", (req, res) => {
       }
     }
     if (full_room.length == all_key.length) {
-      console.log(2);
-      res.status(500).send('ไม่ว่าง');
+      res.status(500).send('[ws-serverAPI]: Room is not empty');
     } else {
-      console.log(3);
-      res.status(200).send('ว่าง');
+      res.status(200).send('[ws-serverAPI]: Pass');
     }
+  }
+});
+
+app.get("/msg/new/:id", (req, res) => {
+  let room_id = req.params.id;
+  if (!(room_id in tempData)) {
+    if (get_one_person_room().length > 1) {
+      res.status(200).send(get_one_person_room());
+    }
+    else {
+      res.status(201).send('[ws-serverAPI]: Pass create new room');
+    }
+  } else {
+    res.status(500).send('[ws-serverAPI]: Cannot create room');
   }
 });
 
@@ -63,41 +74,58 @@ app.get("/msg/:id", (req, res) => {
 
 
 wss.on('connection', (ws, req) => {
-  let roomCH = parse(req.url, true).pathname.split('@')[1];
-  console.log(`[ws-server] User is connect to CH ${roomCH}`);
+  /*          URL stucture
+  ws://domain:port/msg/channel_new!name=Z0TEExt@A0B1C2D
+
+  _create !name=      @room    
+      |     |           |
+  _create!name=Z0TEExt@A0B1C2D  
+    
+  */
+
+  let roomCH = parse(req.url, true).pathname.split('@')[1].toString();
+  let createRoom = parse(req.url, true).pathname.split('create')[1];
+  let userName = parse(req.url, true).pathname.split('name=')[1].split('@')[0];
 
   if (!(roomCH in tempData)) {
-    tempData[roomCH] = {};
-    tempCheck[roomCH] = {};
+    if (createRoom != undefined) {
+      tempData[roomCH] = {};
+      tempCheck[roomCH] = {};
 
-    for (let i in all_key) {
-      tempData[roomCH][all_key[i]] = [];
-      tempCheck[roomCH][all_key[i]] = false;
+      for (let i in all_key) {
+        tempData[roomCH][all_key[i]] = [];
+        tempCheck[roomCH][all_key[i]] = false;
+      }
+    } else {
+      return ws.close();
     }
   }
 
   let full_room = [];
   for (let i in all_key) {
     if (tempData[roomCH][all_key[i]].length != 0) {
-      full_room.push(all_key[i])
+      full_room.push(all_key[i]);
     }
   }
 
   let empty_room = all_key.filter(x => !full_room.includes(x));
-  let target = empty_room[Math.floor(empty_room.length * Math.random())]
+  let target = empty_room[Math.floor(empty_room.length * Math.random())];
 
   if (tempData[roomCH][target] == undefined) {
-    ws.close();
+    return ws.close();
   } else {
     tempData[roomCH][target].push(ws);
     tempCheck[roomCH][target] = true;
 
     const clientId = uuidv4();
     ws.clientId = clientId;
-    ws.send(JSON.stringify({ "id": clientId, "pos": tempCheck[roomCH] }));
+    ws.clientName = userName;
+    ws.send(JSON.stringify({ "id": clientId, "name": userName, "pos": tempCheck[roomCH] }));
+
+    console.log(`[ws-server] ${userName} is connect to CH ${roomCH}`);
   }
 
-  console.log(tempData);
+
 
   ws.on('message', (data, isBinary) => {
     console.log(`[ws ch ${roomCH}] : ${data}`);
@@ -112,6 +140,8 @@ wss.on('connection', (ws, req) => {
   });
 
   ws.on('close', () => {
+    let userInroom = 0;
+
     for (let i in all_key) {
       const index = tempData[roomCH][all_key[i]].indexOf(ws);
       if (index !== -1) {
@@ -119,10 +149,42 @@ wss.on('connection', (ws, req) => {
         tempCheck[roomCH][all_key[i]] = false;
         console.log('[ws-server] User is disconnected');
       }
+
+      tempData[roomCH][all_key[i]].forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ "leave": ws.clientName, 'uuid': ws.clientId }));
+          userInroom++;
+        }
+      });
     }
+
+    if (userInroom == 0) {
+      delete tempData[roomCH]
+    }
+
+    console.log(tempData);
   });
 });
 
+function get_one_person_room() {
+  let result = [];
+
+  for (const key in tempData) {
+    let count = 0
+    for (i in all_key) {
+      tempData[key][all_key[i]].forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          count++;
+        }
+      });
+    }
+    if (count >= 1) {
+      result.push(key);
+    }
+  }
+
+  return result;
+}
 
 // ********************************* Server log ********************************* //
 
